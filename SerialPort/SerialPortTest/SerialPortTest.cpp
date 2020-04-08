54b84a5a30089783d57677ede6624cf614ad1c51
00steve <stevemkrenz@gmail.com>
2020-04-07 19:18:56 -0500

    Updated project so it's linked correctly after moving to a new folder.

--- SerialPort/SerialPortTest/SerialPortTest.cpp
+++ SerialPort/SerialPortTest/SerialPortTest.cpp
@@ -1,8 +1,7 @@
 // SerialPortTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <SerialPort.cpp>
+#include "../SerialPort/SerialPort.h"
 
 int main()
 {
